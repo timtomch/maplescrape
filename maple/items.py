fd0737486b685d18c6fd5678d797121f96ee5b1b
Thomas Guignard <tom@timtom.ch>
2015-04-02 23:06:35 -0400

    Can successfully scrape data
    Still missing clean address details. Now stored in blurb field.

--- maple/items.py
+++ maple/items.py
@@ -5,16 +5,18 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
 
-class MapleItem(scrapy.Item):
+class MapleItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-	region = scrapy.Field()
-	name = scrapy.Field()
-	address = scrapy.Field()
-	phone = scrapy.Field()
-	web = scrapy.Field()
-	email = scrapy.Field()
-    pass
+	region = Field()
+	name = Field()
+	address = Field()
+	postalcode = Field()
+	blurb = Field()
+	phone = Field()
+	web = Field()
+	email = Field()
+
--- maple/spiders/maple_spider.py
+++ maple/spiders/maple_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+import re
+from maple.items import MapleItem
 
 class MapleSpider(scrapy.Spider):
 	name = "maple"
@@ -6,6 +8,46 @@ class MapleSpider(scrapy.Spider):
 	start_urls = ["http://www.ontariomaple.com/pages/farmgate/"]
 	
 	def parse(self, response):
-		for sel in response.xpath('//map/area/@href').extract():
-			print sel
-		
+		for url in response.xpath('//map/area/@href').extract():
+			yield scrapy.Request(url, callback=self.get_data)
+	
+	def get_data(self, response):
+		region = response.xpath('//h1[@class="pageHeader"]/text()').extract()
+		#items = []
+		for listing in response.xpath('//div[@class="directory looking2"]'):
+			item = MapleItem()
+			item['region'] = region
+			item['name'] = listing.xpath('h4/text()').extract()
+			for link in listing.xpath('*/a/@href').extract():
+				pieces = link.split("mailto:",1)
+				if len(pieces) == 2:
+					item['email'] = pieces[1]
+				else:
+					item['web'] = pieces[0]
+			addressfound = None
+			address = ""
+			blurb = ""
+			for needle in listing.xpath('.//text()').extract():
+				blurb = blurb+needle
+				blurb = blurb+"\n"
+				#print needle
+				if addressfound == True:
+					address = address + "\n" + needle
+					item['address'] = address
+					addressfound = None
+				else:
+					match = re.search("[0-9]{3}(-| )[0-9]{3}(-| )[0-9-]*", needle)
+					if match is not None:
+						item['phone'] = match.group()
+					match = re.match("[0-9]+ [A-Z]", needle)
+					if match is not None:
+						#print "found address: ", needle
+						addressfound = True
+						address = needle
+					match = re.search("[A-Z]{1}[0-9]{1}[A-Z]{1} [0-9]{1}[A-Z]{1}[0-9]{1}", needle)
+					if match is not None:
+						item['postalcode'] = match.group()
+				item['blurb'] = blurb
+			#items.append(item)
+			#print item['region'], ' - ', item['name']
+			yield item
